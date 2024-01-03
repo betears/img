6b788ae9409face33d4c50df235ea26c3099f9d0
blog <fantseer@gmail.com>
2024-01-03 11:07:46 +0800

    Update upload.js

--- asset/js/upload.js
+++ asset/js/upload.js
@@ -221,16 +221,16 @@ function render_uploaded() {
         const BASE_PROXYURL = PROXYURL ? PROXYURL : BASE_URL;
         
         var url = resp.data.thumb == null ? BASE_PROXYURL + resp.data.url : BASE_PROXYURL + resp.data.thumb.url;
-        // var wpurl = resp.data.thumb == null ? WPRAW + resp.data.url : WPRAW + resp.data.thumb.url;
-	// var wpcfurl = resp.data.thumb == null ? WPCFRAW + resp.data.url : WPCFRAW + resp.data.thumb.url;
+        var wpurl = resp.data.thumb == null ? WPRAW + resp.data.url : WPRAW + resp.data.thumb.url;
+	var wpcfurl = resp.data.thumb == null ? WPCFRAW + resp.data.url : WPCFRAW + resp.data.thumb.url;
         var RAW = resp.data.thumb == null ? PhRAW + resp.data.url : PhRAW + resp.data.thumb.url;
         $('#imagedetail').append(formatHtml({ url: url, code: url, wp: wpurl, wpcf: wpcfurl, raw: RAW }));
-        $('#htmlcode').append(formatHtml({ url: url, code: '<img src="' + url + '" />', raw: '<img src="' + RAW + '" />' }));
-	// wp: '<img src="' + wpurl + '" />', wpcf: '<img src="' + wpcfurl + '" />',暂未添加
-        $('#bbcode').append(formatHtml({ url: url, code: '[img]' + url + '[/img]',  raw: '[img]' + RAW + '[/img]' }));
-	// wp: '[img]' + wpurl + '[/img]' , wpcf: '[img]' + wpcfurl + '[/img]' ,暂未添加
-        $('#markdown').append(formatHtml({ url: url, code: '![' + name + '](' + url + ')', raw: '![' + name + '](' + RAW + ')' }));
-	// wp: '![' + name + '](' + wpurl + ')', wpcf: '![' + name + '](' + wpcfurl + ')', 暂未添加
+        $('#htmlcode').append(formatHtml({ url: url, code: '<img src="' + url + '" />',wp: '<img src="' + wpurl + '" />', wpcf: '<img src="' + wpcfurl + '" />', raw: '<img src="' + RAW + '" />' }));
+	// 暂未添加
+        $('#bbcode').append(formatHtml({ url: url, code: '[img]' + url + '[/img]', wp: '[img]' + wpurl + '[/img]' , wpcf: '[img]' + wpcfurl + '[/img]' , raw: '[img]' + RAW + '[/img]' }));
+	// 暂未添加
+        $('#markdown').append(formatHtml({ url: url, code: '![' + name + '](' + url + ')', wp: '![' + name + '](' + wpurl + ')', wpcf: '![' + name + '](' + wpcfurl + ')', raw: '![' + name + '](' + RAW + ')' }));
+	// 暂未添加
     });
 }
 
